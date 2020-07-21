d9faef6b0517f95048a53996863615b684e8b73a
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-07-22 02:32:07 +0800

    remove redundant code

--- src/index.js
+++ src/index.js
@@ -1,25 +1,5 @@
 var numbers = [3, 56, 2, 48, 5];
 
-//Filter - Create a new array by keeping the items that return true.
-
-// method 1 - filter
-const newNumbers = numbers.filter(function (num) {
-  return num < 10
-})
-
-console.log(newNumbers)
-
-// method 2 - forEach
-
-const feNewNumbers = []
-
-numbers.forEach(function (num) {
-  if (num < 10) {
-    feNewNumbers.push(num)
-  }
-})
-
-console.log(feNewNumbers)
 //Reduce - Accumulate a value by doing something to each item in an array.
 
 //Find - find the first item that matches from an array.
