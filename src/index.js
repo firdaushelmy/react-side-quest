2e65197e04d64ec6dd494faf0c3f9bb94cfb13fe
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-07-23 00:28:57 +0800

    add reduce method

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ var numbers = [3, 56, 2, 48, 5];
 
 //Reduce - Accumulate a value by doing something to each item in an array.
 
+// method 1 - Reduce method. the name reduce means reducing an array to a single value.
+
+const reduceNum = numbers.reduce(function (accumulator, currentNumber) {
+  return accumulator + currentNumber;
+})
+
+console.log(reduceNum)
 
 // method 2 - forEach
 let newNumber = 0;
